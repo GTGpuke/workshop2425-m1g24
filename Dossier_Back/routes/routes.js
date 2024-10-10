ab45de18be5d2555f190064023c2c4c8d50c0e57
Pierre <oui>
2024-10-10 02:47:46 +0200

    Il manque médecin

--- Dossier_Back/routes/routes.js
+++ Dossier_Back/routes/routes.js
@@ -4,6 +4,7 @@ const router = express.Router(); // Créer un routeur Express
 // Importer tous les contrôleurs nécessaires
 const medecinController = require('../controller/medecinController'); // Contrôleur pour les médecins
 const patientController = require('../controller/patientController'); // Contrôleur pour les patients
+const adresseController = require('../controller/adresseController'); // Contrôleur pour les adresses
 
 // Ajoutez d'autres contrôleurs si nécessaire
 
@@ -20,6 +21,20 @@ router.get('/patients/:id', patientController.getPatientById); // Obtenir un pat
 router.post('/patients', patientController.createPatient); // Créer un nouveau patient
 router.put('/patients/:id', patientController.updatePatient); // Mettre à jour un patient par ID
 router.delete('/patients/:id', patientController.deletePatient); // Supprimer un patient par ID
+
+// Routes pour les patients
+router.post('/adresses/patient', adresseController.createAdressePatient); // Créer une nouvelle adresse pour un patient
+router.get('/adresses/patients', adresseController.getAllAdressesPatients); // Récupérer toutes les adresses des patients
+router.get('/adresses/patient/:id', adresseController.getAdressesPatient); // Récupérer toutes les adresses d'un patient
+router.put('/adresses/patient/:id', adresseController.updateAdressePatient); // Mettre à jour une adresse d'un patient
+router.delete('/adresses/patient/:id', adresseController.deleteAdressePatient); // Supprimer une adresse d'un patient
+
+// Routes pour les médecins
+router.post('/adresses/medecin', adresseController.createAdresseMedecin); // Créer une nouvelle adresse pour un médecin
+router.get('/adresses/medecins', adresseController.getAllAdressesMedecins); // Récupérer toutes les adresses des médecins
+router.get('/adresses/medecin/:id', adresseController.getAdressesMedecin); // Récupérer toutes les adresses d'un médecin
+router.put('/adresses/medecin/:id', adresseController.updateAdresseMedecin); // Mettre à jour une adresse d'un médecin
+router.delete('/adresses/medecin/:id', adresseController.deleteAdresseMedecin); // Supprimer une adresse d'un médecin
 
 // Ajoutez d'autres routes pour les maladies, traitements, etc. selon vos besoins
 
